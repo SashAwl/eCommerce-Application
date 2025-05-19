b5cd89f7628a7e54122dbeb03c3d913076c19c6a
Alina M <crlin.m@gmail.com>
2025-05-20 03:44:26 +0500

    feat: finish part with fields validation

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import './LoginPage.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import apiRoot from '../../utils/sdkClient';
@@ -28,9 +27,6 @@ const loginSchema = z.object({
 type LoginFormData = z.infer<typeof loginSchema>;
 
 export function LoginPage() {
-    const refLogin = useRef<HTMLInputElement>(null);
-    const refPassword = useRef<HTMLInputElement>(null);
-
     const navigate = useNavigate();
     const gameStore = useGameStore();
 
@@ -46,81 +42,76 @@ export function LoginPage() {
             rememberMe: false,
         },
     });
-    console.log(register, handleSubmit);
 
-    const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
-        const email = refLogin.current!.value;
-        const password = refPassword.current!.value;
-
+    const onSubmit = async (data: LoginFormData) => {
         const loginData: MyCustomerSignin = {
-            email,
-            password,
+            email: data.email,
+            password: data.password,
         };
 
-        void (async () => {
-            try {
-                const response = await apiRoot
-                    .me()
-                    .login()
-                    .post({
-                        body: loginData,
-                    })
-                    .execute();
-                await navigate('/');
-                gameStore.login();
-                gameStore.setSuccessMessage('User successfully logged in');
-                setTimeout(() => {
-                    gameStore.clearSuccessMessage();
-                }, 2000);
-                console.log(response.body);
-            } catch (e: unknown) {
-                const error = e as ErrorObject;
-                console.error('Login error:', error);
-                let message = 'An unknown error occurred during login.';
-                const errBody = error.body as
-                    | {
-                          message: string;
-                          errors: {
-                              message?: string;
-                              code: string;
-                          }[];
-                      }
-                    | undefined;
-                if (errBody?.message) {
-                    message = errBody.message;
-                } else if (error.message) {
-                    message = error.message;
-                }
-
-                if (
-                    error.body &&
-                    Array.isArray(errBody?.errors) &&
-                    errBody.errors.length > 0
-                ) {
-                    const ctError = errBody.errors[0];
-                    if (ctError.code === 'InvalidCredentials') {
-                        message =
-                            'Invalid email or password. Please try again.';
-                    } else {
-                        message = ctError.message ?? message;
-                    }
-                } else if (error.statusCode === 400) {
-                    message = 'Invalid email or password provided.';
-                }
-
-                gameStore.setErrorMessage(message);
-                setTimeout(() => {
-                    gameStore.clearErrorMessage();
-                }, 2000);
+        try {
+            const response = await apiRoot
+                .me()
+                .login()
+                .post({
+                    body: loginData,
+                })
+                .execute();
+            await navigate('/');
+            gameStore.login();
+            gameStore.setSuccessMessage('User successfully logged in');
+            setTimeout(() => {
+                gameStore.clearSuccessMessage();
+            }, 2000);
+            console.log(response.body);
+        } catch (e: unknown) {
+            const error = e as ErrorObject;
+            console.error('Login error:', error);
+            let message = 'An unknown error occurred during login.';
+            const errBody = error.body as
+                | {
+                      message: string;
+                      errors: {
+                          message?: string;
+                          code: string;
+                      }[];
+                  }
+                | undefined;
+            if (errBody?.message) {
+                message = errBody.message;
+            } else if (error.message) {
+                message = error.message;
             }
-        })();
+
+            if (
+                error.body &&
+                Array.isArray(errBody?.errors) &&
+                errBody.errors.length > 0
+            ) {
+                const ctError = errBody.errors[0];
+                if (ctError.code === 'InvalidCredentials') {
+                    message = 'Invalid email or password. Please try again.';
+                } else {
+                    message = ctError.message ?? message;
+                }
+            } else if (error.statusCode === 400) {
+                message = 'Invalid email or password provided.';
+            }
+
+            gameStore.setErrorMessage(message);
+            setTimeout(() => {
+                gameStore.clearErrorMessage();
+            }, 2000);
+        }
     };
 
     return (
         <div className="auth__container">
             <div className="auth_form__container">
-                <form className="auth_form" action="">
+                <form
+                    className="auth_form"
+                    onSubmit={(e) => void handleSubmit(onSubmit)(e)}
+                >
                     <h2>Sign in or create account</h2>
 
                     <div className="form_group">
@@ -131,10 +122,16 @@ export function LoginPage() {
                             id="email"
                             type="email"
                             placeholder="Enter your email"
-                            ref={refLogin}
                             className={`form_input ${errors.email ? 'error' : ''}`}
-                            // {...register('email')}
+                            {...register('email')}
                         />
+                        {errors.email && (
+                            <div className="formError">
+                                {errors.email.message}
+                            </div>
+                        )}
+                    </div>
+                    <div className="form_group">
                         <label className="form_label" htmlFor="password">
                             Password
                         </label>
@@ -142,11 +139,16 @@ export function LoginPage() {
                             id="password"
                             type="password"
                             placeholder="Enter your password"
-                            className="form_input"
-                            ref={refPassword}
+                            className={`form_input ${errors.password ? 'error' : ''}`}
+                            {...register('password')}
                         />
+                        {errors.password && (
+                            <div className="form_error">
+                                {errors.password.message}
+                            </div>
+                        )}
                     </div>
-                    <button className="btn_primary" onClick={onSubmit}>
+                    <button className="btn_primary" type="submit">
                         Sign In
                     </button>
 
