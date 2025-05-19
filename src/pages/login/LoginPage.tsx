24d4a57dd27c83652122501881911b1b1aa31445
Alina M <crlin.m@gmail.com>
2025-05-19 13:12:27 +0500

    feat: add styles for login page

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './LoginPage.scss';
 
 export function LoginPage(props: {
     onLogin?: (login: string, password: string) => void;
@@ -6,41 +7,55 @@ export function LoginPage(props: {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     return (
-        <div>
-            <form action="">
-                <h2>Sign in or create account</h2>
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    type="email"
-                    value={login}
-                    placeholder="Enter your email"
-                    onInput={(e) =>
-                        setLogin((e.target as HTMLInputElement).value)
-                    }
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    type="password"
-                    value={password}
-                    placeholder="Enter your password"
-                    onInput={(e) =>
-                        setPassword((e.target as HTMLInputElement).value)
-                    }
-                />
-                <button
-                    type="submit"
-                    onClick={() => props.onLogin?.(login, password)}
-                >
-                    Sign In
-                </button>
+        <div className="auth__container">
+            <div className="auth_form__container">
+                <form className="auth_form" action="">
+                    <h2>Sign in or create account</h2>
 
-                <div>
-                    <a href="#forgot-password">Forgot password?</a>
-                    <a href="#">Create account</a>
-                </div>
-            </form>
+                    <div className="form_group">
+                        <label className="form_label" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Enter your email"
+                            className="form_input"
+                            value={login}
+                            onInput={(e) =>
+                                setLogin((e.target as HTMLInputElement).value)
+                            }
+                        />
+                        <label className="form_label" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            id="password"
+                            type="password"
+                            placeholder="Enter your password"
+                            className="form_input"
+                            value={password}
+                            onInput={(e) =>
+                                setPassword(
+                                    (e.target as HTMLInputElement).value
+                                )
+                            }
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn_primary"
+                        onClick={() => props.onLogin?.(login, password)}
+                    >
+                        Sign In
+                    </button>
+
+                    <div className="form_footer">
+                        <a href="#forgot-password">Forgot password?</a>
+                        <a href="#">Create account</a>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
