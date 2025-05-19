21927c8e9654dc379ac505b59e811b956a5a4c8b
alexpodelinskii <nikitik00787@gmail.com>
2025-05-19 19:29:48 +0300

    feat: add redirect if customer login

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,15 +3,24 @@ import HomePage from '../pages/home/Home';
 import NotFoundPage from '../pages/not-found/NotFoundPage';
 import Registration from '../pages/registration/Registration';
 import LoginPage from '../pages/login/Login';
+import { useGameStore } from '../store/store';
 
 export default function Router() {
+    const isLogin = useGameStore((state) => state.isLogin);
+
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/registration" element={<Registration />} />
+            <Route
+                path="/registration"
+                element={!isLogin ? <LoginPage /> : <Registration />}
+            />
             <Route path="/home" element={<HomePage />} />
             <Route path="/not-found" element={<NotFoundPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route
+                path="/login"
+                element={!isLogin ? <LoginPage /> : <HomePage />}
+            />
             <Route path="/*" element={<NotFoundPage />} />
         </Routes>
     );
