44f7a3f8d9daac2d24d02fd05389657d8563a5af
alexpodelinskii <nikitik00787@gmail.com>
2025-05-26 00:24:12 +0300

    refactor: split the registration page into components

---
+++ src/components/registration/InputField.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    label: string;
+    inputId: string;
+    error?: string;
+    errorMessage?: string;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+    inputId,
+    label,
+    error,
+    ...props
+}) => {
+    return (
+        <div className={'formGroup'}>
+            <label htmlFor={inputId} className={'formLabel'}>
+                {label}
+            </label>
+            <input
+                id={inputId}
+                className={`formInput ${error ? 'error' : ''} `}
+                {...props}
+            />
+            {error && <div className="formError">{error}</div>}
+        </div>
+    );
+};
+
+export default InputField;
