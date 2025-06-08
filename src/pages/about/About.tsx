06da58343364833b53558d1070cd347cda505266
Alina M <crlin.m@gmail.com>
2025-06-08 15:05:57 +0500

    feat: add title for about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,8 +1,20 @@
+import './About.scss';
+
 const About = () => {
     return (
-        <>
-            <p>In development...</p>
-        </>
+        <div className="about">
+            <div className="about__inner">
+                <div className="about__title">
+                    <h1 className="about__heading">About Our Team</h1>
+                    <p className="about__description">
+                        We're a passionate team of developers committed to
+                        building the best gaming marketplace. Our goal is to
+                        connect gamers with their favorite titles through a
+                        user-friendly and powerful platform.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 };
 
