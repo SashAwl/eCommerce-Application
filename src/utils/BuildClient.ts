a49c064919242db60575af7f9c43c782d18d84a2
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-07-07 11:51:18 +0300

    fix: restore removed types

--- src/utils/BuildClient.ts
+++ src/utils/BuildClient.ts
@@ -36,35 +36,3 @@ export const ctpClient = new ClientBuilder()
     .withClientCredentialsFlow(authMiddlewareOptions)
     .withHttpMiddleware(httpMiddlewareOptions)
     .build();
-
-// import {
-//     ClientBuilder,
-
-//     // Import middlewares
-//     type AuthMiddlewareOptions, // Required for auth
-//     type HttpMiddlewareOptions, // Required for sending HTTP requests
-// } from '@commercetools/ts-client';
-
-// // Configure authMiddlewareOptions
-// const authMiddlewareOptions: AuthMiddlewareOptions = {
-//     host: 'https://auth.australia-southeast1.gcp.commercetools.com',
-//     projectKey: 'mergemates',
-//     credentials: {
-//         clientId: 'dZnENdU2BB32IKq7Bc_0AlsW',
-//         clientSecret: 'WYfOviHMQFxXFNtWurOwi_wBkZKTUHvp',
-//     },
-//     scopes: ['manage_project:mergemates'],
-//     httpClient: fetch,
-// };
-
-// // Configure httpMiddlewareOptions
-// const httpMiddlewareOptions: HttpMiddlewareOptions = {
-//     host: 'https://api.australia-southeast1.gcp.commercetools.com',
-//     httpClient: fetch,
-// };
-
-// // Export the ClientBuilder
-// export const ctpClient = new ClientBuilder()
-//     .withClientCredentialsFlow(authMiddlewareOptions)
-//     .withHttpMiddleware(httpMiddlewareOptions)
-//     .build();
